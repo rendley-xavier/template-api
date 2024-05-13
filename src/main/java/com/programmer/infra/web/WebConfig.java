0b2085b3f44b5dd6510f02f009acb9106321b692
Rendley Arnou Xavier <rendley.xavier@sps.ce.gov.br>
2024-05-13 09:40:27 -0300

    feat: Adiciona configuaração do cors

---
+++ src/main/java/com/programmer/infra/web/WebConfig.java
@@ -0,0 +1,23 @@
+package com.programmer.infra.web;
+
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
+    }
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+
+}
