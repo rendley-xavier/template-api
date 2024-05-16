02e4af9cc15bdd90f129fa83088c24e3a3573f22
Rendley Arnou Xavier <rendley.xavier@sps.ce.gov.br>
2024-05-16 13:53:49 -0300

    fix: Resolve conflito com importação do pacote da classe GenericDisassembler

--- src/main/java/com/programmer/api/disassembler/GenericDisassembler.java
+++ src/main/java/com/programmer/api/disassembler/GenericDisassembler.java
@@ -1,7 +1,5 @@
-package br.gov.ce.sps.sqp.api.disassembler;
+package com.programmer.api.disassembler;
 
-import br.gov.ce.sps.sqp.api.input.*;
-import br.gov.ce.sps.sqp.domain.model.*;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -15,6 +13,7 @@ public class GenericDisassembler {
 	public <T, U> U toDomainObject(T entity, Class<U> modelClass) {
 		return modelMapper.map(entity, modelClass);
 	}
+
 	public <T, U> void copyToDomainObject(T input, U domainObject) {
 
 		modelMapper.map(input, domainObject);
